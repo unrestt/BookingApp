438f42ea55bb65a1aa23d403c9050ff10d910d10
Bartosz Kutnik <kutnik.contact@gmail.com>
2026-06-07 20:07:40 +0200

    feat: initialize frontend project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created initial App component with routing setup.
    - Implemented CustomToaster component for toast notifications.
    - Added MainLayout and PageNotFound components for layout structure.
    - Configured TypeScript with tsconfig files for app and node environments.
    - Set up Vite configuration with React and Tailwind CSS plugins.
    - Included global CSS file for Tailwind CSS styles.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import CustomToaster from './components/CustomToaster'
+import MainLayout from './layout/MainLayout'
+import PageNotFound from './layout/PageNotFound'
+
+function App() {
+
+  return (
+    <>
+      <CustomToaster/>
+      <Routes>
+        <Route path='/' element={<MainLayout/>}/>
+        <Route path='*' element={<PageNotFound/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
